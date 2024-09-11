e057b3118c38db66c3e45c01f43e99d022c892ca
Thanh Vinh <ptvinh203@gmail.com>
2024-09-12 00:31:19 +0700

    feat: done preprocess data

--- hate_speech_text_span_detection/preprocess/tokenize_by_word.py
+++ hate_speech_text_span_detection/preprocess/tokenize_by_word.py
@@ -1,32 +1,119 @@
+#
+# PLEASE DOWNLOAD VNCoreNLP WITH AVALIABLE SCRIPT IN SCRIPTS FOLDER
+#
+
+import numpy as np
 import pandas as pd
 from ast import literal_eval
 from IPython.core.interactiveshell import InteractiveShell
-from utils import get_path, norm_unicode
+from utils import (
+    get_path,
+    norm_unicode,
+    preprocess,
+    find_consecutive_ranges,
+    tokenize_word,
+    annotate,
+)
 
 InteractiveShell.ast_node_interactivity = "all"
 
 
-# Pandas settings
+# Pandas global settings
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", None)
 pd.set_option("display.max_colwidth", None)
 pd.options.display.max_rows
 
-# Pandas load data
-data_dir = get_path("hate_speech_text_span_detection/data/raw", "")
+# Pandas load raw data
+raw_train, raw_dev, raw_test = None, None, None
+raw_data_dir = get_path("hate_speech_text_span_detection/data/raw", "")
 
-train = pd.read_csv(f"{data_dir}/train.csv")
-train["index_spans"] = train["index_spans"].apply(literal_eval)
-norm_unicode(train)
 
-dev = pd.read_csv(f"{data_dir}/dev.csv")
-dev["index_spans"] = dev["index_spans"].apply(literal_eval)
-norm_unicode(dev)
+def load_raw_data():
+    global raw_train, raw_dev, raw_test
+    raw_train = pd.read_csv(f"{raw_data_dir}/train.csv")
+    raw_train["index_spans"] = raw_train["index_spans"].apply(literal_eval)
+    norm_unicode(raw_train)
 
-test = pd.read_csv(f"{data_dir}/test.csv")
-test["index_spans"] = test["index_spans"].apply(literal_eval)
-norm_unicode(test)
+    raw_dev = pd.read_csv(f"{raw_data_dir}/dev.csv")
+    raw_dev["index_spans"] = raw_dev["index_spans"].apply(literal_eval)
+    norm_unicode(raw_dev)
+
+    raw_test = pd.read_csv(f"{raw_data_dir}/test.csv")
+    raw_test["index_spans"] = raw_test["index_spans"].apply(literal_eval)
+    norm_unicode(raw_test)
+
+
+def load_text_spans_from_raw_data(raw_data: pd.DataFrame) -> list:
+    data = []
+    for _, row in raw_data.iterrows():
+        text = row["content"]
+        idx_spans = row["index_spans"]
+        temp = []
+        text_spans = []
+        if idx_spans:
+            segments = find_consecutive_ranges(idx_spans)
+            for seg in segments:
+                if len(seg) == 2 and seg[0] == seg[1]:  # single word
+                    temp.append([seg[0]])
+                    text_spans.append(
+                        text[seg[0] : seg[-1] + 1]
+                    )  # substring of text from seg[0] to seg[-1] + 1
+                else:
+                    temp.append([seg[0], seg[-1]])
+                    text_spans.append(
+                        text[seg[0] : seg[-1] + 1]
+                    )  # substring of text from seg[0] to seg[-1] + 1
+        data.append({"text": text, "index_HOS_spans": temp, "text_spans": text_spans})
+    return data
+
+
+def process_BIO_data(data: list) -> pd.DataFrame:
+    formated_data = []
+    for d in data:
+        text = d["text"]
+        pos = [i for i in range(len(text))]
+        text, pos = preprocess(text, pos)
+        tokens, alignment = tokenize_word(text, pos)
+        annotations = annotate(d["index_HOS_spans"], alignment, tokens)
+        ls = [[tokens[i], annotations[i]] for i in range(len(tokens))]
+
+        formated_data.extend(ls)
+        formated_data.append([None])  # add None to separate records
+    df_final = pd.DataFrame(formated_data, columns=["Word", "Tag"])
+    sentence_id = []
+    sentence = 0  # sentence id is ordered from 0
+    for i in range(len(df_final)):
+        if df_final["Word"][i] != None:  # if not None, add sentence id
+            sentence_id.append(sentence)
+        else:  # if None (separate record data), add None to sentence id and increase sentence id
+            sentence_id.append(np.nan)
+            sentence += 1
+    df_final["sentence_id"] = sentence_id
+    df_final.dropna(inplace=True)  # drop None records
+    df_final["sentence_id"] = df_final["sentence_id"].astype("int64")
+    return df_final
+
+
+def save_BIO_data():
+    save_path = get_path("hate_speech_text_span_detection/data/processed", "")
+
+    BIO_train = process_BIO_data(load_text_spans_from_raw_data(raw_train))
+    BIO_dev = process_BIO_data(load_text_spans_from_raw_data(raw_dev))
+    BIO_test = process_BIO_data(load_text_spans_from_raw_data(raw_test))
+
+    BIO_train.reset_index(inplace=True)
+    BIO_dev.reset_index(inplace=True)
+    BIO_test.reset_index(inplace=True)
+
+    # Save to csv
+    BIO_train.to_csv(f"{save_path}/train.csv")
+    BIO_dev.to_csv(f"{save_path}/dev.csv")
+    BIO_test.to_csv(f"{save_path}/test.csv")
+
 
 if __name__ == "__main__":
-    print(test.head(2))
+    load_raw_data()
+    save_BIO_data()
+    print("Preprocess data successfully!")
--- hate_speech_text_span_detection/preprocess/utils.py
+++ hate_speech_text_span_detection/preprocess/utils.py
@@ -1,3 +1,6 @@
+from itertools import groupby
+from operator import itemgetter
+from vncorenlp import VnCoreNLP
 import os
 import re
 import numpy as np
@@ -23,6 +26,14 @@ def get_path(
     return file_path
 
 
+# Initialize annotator
+annotator = VnCoreNLP(
+    get_path("hate_speech_text_span_detection/vncorenlp", "VnCoreNLP-1.1.1.jar"),
+    annotators="wseg",
+    max_heap_size="-Xmx500m",
+)
+
+
 def norm_unicode(df: pd.DataFrame) -> pd.DataFrame:
     col_name = "content"
     for row_idx in range(len(df)):
@@ -46,24 +57,26 @@ def norm_unicode(df: pd.DataFrame) -> pd.DataFrame:
 
 
 # Replace text with pattern by replacement and keep track of the position
+# Ex: replace("Hello, world!!!", "o", "a", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14])
+#    -> ("Hella, warld!!!", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14])
 def replace(
     text: str, pattern: str, replacement: str, pos: List[int]
 ) -> Tuple[str, List[int]]:
-    matches = [0]
+    match_indices = [0]  # start from first index
 
     def capture_and_replace(match: re.Match, ret: str) -> str:
-        matches.extend([match.start() + 1, match.end()])
+        match_indices.extend([match.start() + 1, match.end()])
         return ret
 
-    l = len(text)
+    last_idx = len(text)
     text = re.sub(
         pattern,
         lambda match: capture_and_replace(match, replacement),
         text,
         flags=re.IGNORECASE,
     )
-    matches.append(l)
-    slices = np.array_split(matches, int(len(matches) / 2))
+    match_indices.append(last_idx)  # append the last index
+    slices = np.array_split(match_indices, int(len(match_indices) / 2))
     res = []
     for s in slices:
         res += pos[s[0] : s[1]]
@@ -71,6 +84,8 @@ def replace(
     return text, res
 
 
+# Preprocess text by collapsing duplicated punctuations
+# Ex: "Hello, world!!!" -> "Hello, world!"
 def preprocess(text: str, pos: List[int]) -> Tuple[str, List[int]]:
     # collapse duplicated punctuations
     punc = ",. !?\"'"
@@ -79,3 +94,77 @@ def preprocess(text: str, pos: List[int]) -> Tuple[str, List[int]]:
         text, pos = replace(text, pat, c, pos)
     assert len(text) == len(pos)
     return text, pos
+
+
+# Find ranges of consecutive numbers
+# Ex: [1, 2, 3, 5, 6, 7, 9] -> [(1, 3), (5, 7), (9, 9)]
+def find_consecutive_ranges(numbers: List[int]) -> List[Tuple[int, int]]:
+    ranges = []
+    for _, group in groupby(
+        enumerate(numbers), lambda x: x[0] - x[1]
+    ):  # groupby consecutive numbers
+        consecutive_group = list(map(itemgetter(1), group))
+        ranges.append((consecutive_group[0], consecutive_group[-1]))
+    return ranges
+
+
+# Tokenize text by word
+# Ex: tokenize_word("Hay quá Mac _. Lê- Nin", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21])
+#     -> (['Hay', 'quá', 'Mac', '_', '.', 'Lê', '-', 'Nin'], [[0, 1, 2], [4, 5, 6], [8, 9, 10], [12], [13], [15, 16], [17], [19, 20, 21]])
+def tokenize_word(text: str, pos: List[int]) -> Tuple[List[str], List[List[int]]]:
+    annotator_text = annotator.tokenize(text)
+    tokens = []
+    for i in range(len(annotator_text)):
+        for j in range(len(annotator_text[i])):
+            if annotator_text[i][j] == "_":  # add "_" to tokens
+                tokens.append(annotator_text[i][j])
+            else:  # remove "_" from annotator_text, then split by "_" and add all syllables to tokens
+                annotator_text[i][j] = annotator_text[i][j].lstrip("_")
+                annotator_text[i][j] = annotator_text[i][j].rstrip("_")
+                syllabel = annotator_text[i][j].split("_")
+                for idx in range(len(syllabel)):
+                    tokens.append(syllabel[idx])
+    alignment = []
+    start = 0
+    for t in tokens:  # find and group positions of each token in the original text
+        res = text.find(t, start)
+        alignment.append(pos[res : res + len(t)])
+        start = res + len(t)
+    assert len(tokens) == len(alignment)
+    return tokens, alignment
+
+
+# Annotate tokens by BIO schema
+# Explain:
+#   B-T: Begin of token
+#   I-T: Inside of token
+#   O  : Outside of token
+def annotate(
+    index_HOS_spans: List[List[int]], alignment: List[int], tokens: List[str]
+) -> pd.Series:
+    Tag = []
+    annotations = pd.DataFrame()
+    annotations["Tokens"] = tokens
+    for i in range(len(tokens)):
+        Tag.append("O")
+    annotations["Tag"] = Tag
+    for indices in index_HOS_spans:
+        i = 0
+        while i < len(alignment):
+            if (
+                alignment[i][-1] < indices[0]
+            ):  # if the last index of alignment[i] is less than the first index of indices => outside of token
+                i += 1
+            elif (
+                alignment[i][0] <= indices[0] <= alignment[i][-1]
+            ):  # if the first index of indices is in alignment[i] => begin of token
+                annotations.loc[i, "Tag"] = "B-T"
+                i += 1
+            elif (
+                indices[0] < alignment[i][0] <= indices[-1]
+            ):  # if the first index of alignment[i] is in indices => inside of token
+                annotations.loc[i, "Tag"] = "I-T"
+                i += 1
+            elif alignment[i][0] > indices[-1]:
+                break
+    return annotations["Tag"]
